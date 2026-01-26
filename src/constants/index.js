086288b77943139920eb158a38f5594b63c0eaa1
Tushant <trajpal@hotmail.com>
2026-01-27 01:38:31 +0530

    Added the projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -118,73 +118,27 @@ export const projects = [
       { name: "DotNet C#", color: "blue-text-gradient" },
       {name: "ReactJS", color: "blue-text-gradient"},
       {name: "NodeJS", color: "green-text-gradient"},
-      {name: "AWS", color: "pink-text-gradient"},
-      {name: "Azure", color: "yellow-text-gradient"},
+      {name: "Azure", color: "orange-text-gradient"},
       { name: "Javascript", color: "blue-text-gradient" },
       { name: "HTML", color: "green-text-gradient" },
       { name: "bootstrap 5.3.0", color: "pink-text-gradient" },
     ],
     image: weatherpedia,
     source_code_link: "N/A",
-  },
+  }, 
   {
-    name: "Search Education Portfolio Website",
+    name: "Medical E-Commerce",
     description:
-      "A terminal themed portfolio website that allows users to type into the terminal and use commands like a real terminal.",
+      "Web-based platform that allows users to buy the products in bulk and pay for the delivery. It includes getting the orders from the suppliers and managing the inventory.",
     tags: [
-      { name: "HTML", color: "blue-text-gradient" },
-      { name: "css", color: "green-text-gradient" },
-      { name: "Javascript", color: "pink-text-gradient" },
-    ],
-    image: termpw,
-    source_code_link: "https://github.com/trajpal2/trajpal2.github.io",
-  },
-  {
-    name: "Mental Health Fitness Tracker",
-    description:
-      "ML model that utilizes regression techniques to provide insights into mental health and make predictions based on the available data.",
-    tags: [
-      { name: "Machine Learning", color: "blue-text-gradient" },
-      { name: "Jupyter Notebook", color: "green-text-gradient" },
-      { name: "Regression Algorithms", color: "pink-text-gradient" },
-    ],
-    image: mhft,
-    source_code_link:
-      "https://github.com/trajpal2/mental_health_fitness_tracker",
-  },
-  {
-    name: "PayloadMaster",
-    description:
-      "Tool to automate payload creation using the Metasploit framework",
-    tags: [
-      { name: "shell", color: "blue-text-gradient" },
-    ],
-    image: payloadmaster,
-    source_code_link: "https://github.com/trjapal2/PayloadMaster",
-  },
-  {
-    name: "CompileVortex",
-    description:
-      "Tool to automate payload creation using the Metasploit framework",
-    tags: [
+      {name: "ReactJS", color: "blue-text-gradient"},
+      {name: "NodeJS", color: "green-text-gradient"},
+      {name: "AWS", color: "pink-text-gradient"},
       { name: "Javascript", color: "blue-text-gradient" },
-      { name: "CSS", color: "green-text-gradient" },
-      { name: "HTML", color: "pink-text-gradient" },
+      { name: "HTML", color: "green-text-gradient" },
+      { name: "Tailwind", color: "pink-text-gradient" },
     ],
-    image: CompileVortex,
-    source_code_link: "https://github.com/trajpal2/CompileVortex",
-  },
-  {
-    name: "Sketcher",
-    description:
-      "Convert an input image to a pencil sketch using OpenCV and Matplotlib libraries.",
-    tags: [
-      { name: "OpenCV", color: "blue-text-gradient" },
-      { name: "Matplotlib", color: "green-text-gradient" },
-      { name: "Python", color: "pink-text-gradient" },
-    ],
-
-    image: sketcher,
-    source_code_link: "https://github.com/trajpal2/Image_to_Pencil_Sketch_App",
-  },
+    image: weatherpedia,
+    source_code_link: "N/A", 
+  }
 ];
